c1eda8ee2389afb2b6395bcdfb4dbd1994fecdde
Daniel <42978531+danielhan1@users.noreply.github.com>
2018-10-26 14:29:04 -0400

    Add files via upload

---
+++ src/Dice.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * 
+ * @author dhan19
+ *
+ */
+public class Dice
+{
+			public static void main(String[] args)
+			{
+				DiceMain die1 = new DiceMain();
+				DiceMain die2 = new DiceMain();
+
+				
+				int roll1 = die1.roll();
+				int roll2 = die2.roll();
+				
+				for (int i=0; i<1000; i++)
+				{
+					
+				}
+			}
+}
